55a98086de7708cdad52cd097f5669cc3ac87ead
kathyk90 <69370809+kathyk90@users.noreply.github.com>
2020-08-17 02:28:30 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,9 +159,3 @@ STATICFILES_DIRS = (
     # Add to this list all the locations containing your static files 
 )
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
